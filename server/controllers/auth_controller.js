9c10e7fd720099496ff98cbda9e34a6ec44f7af6
Jonathon Flores <jonathon@Jonathons-MacBook-Pro.local>
2018-12-20 05:22:51 -0600

    completed project

---
+++ server/controllers/auth_controller.js
@@ -0,0 +1,50 @@
+const users = require("../models/users");
+
+let id = 1;
+
+const login = (req, res, next) => {
+  const { session } = req;
+  const { username, password } = req.body;
+
+  const user = users.find(
+    user => user.username === username && user.passowrd === password
+  );
+
+  if (user) {
+    session.user.usernsme = user.username;
+    res.status(200).json(session.user);
+  } else {
+    res.status(500).json("Unauthorized");
+  }
+};
+
+const register = (req, res, next) => {
+  const { session } = req;
+  const { username, password } = req.body;
+
+  users.push({ id, username, password });
+  id++;
+
+  session.user.username = username;
+
+  res.status(200).json(session.user);
+};
+
+const signout = (req, res, next) => {
+  const { session } = req;
+  session.destroy();
+  res.status(200).json(session);
+};
+
+const getUser = (req, res, next) => {
+  const { session } = req;
+
+  res.status(200).json(session.user);
+};
+
+module.exports = {
+  login,
+  register,
+  signout,
+  getUser
+};
